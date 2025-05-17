558fb01c7422d32880d81d153d1a1780471c1199
amogus <amogus@amogus.com>
2025-05-17 16:24:34 +0200

    - „skrypt.sh --logs” utworzy automatycznie 100 plików logx.txt, x – numer pliku od 1 – 100, w każdym pliku należy wpisać jego nazwę, nazwę skryptu który go utworzył i datę

--- skrypt.sh
+++ skrypt.sh
@@ -5,6 +5,16 @@ while test $# -gt 0; do
 			exit 0
 		;;
 
+		--logs)
+			for i in $(seq 1 100);
+			do
+				echo "log$i.txt" > log$i.txt
+				echo "${0##*/}" >> log$i.txt
+				echo "$(date +%d.%m.%Y)" >> log$i.txt
+			done
+			exit 0
+		;;
+
 		*)
 			break
 		;;
