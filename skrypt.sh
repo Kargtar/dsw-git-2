4df6d7dac034974bc44043b9034540ed31e86b2a
amogus <amogus@amogus.com>
2025-05-17 16:41:52 +0200

    - „skrypt.sh --logs 30” utworzenie automatycznie 30 plików, analogicznie jak wyżej, ale z obsługą argumentu liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -6,7 +6,14 @@ while test $# -gt 0; do
 		;;
 
 		--logs)
-			for i in $(seq 1 100);
+			shift
+			if test $# -gt 0; then
+				export fin=$1
+			else
+				export fin=100
+			fi
+			shift
+			for i in $(seq 1 $fin);
 			do
 				echo "log$i.txt" > log$i.txt
 				echo "${0##*/}" >> log$i.txt
